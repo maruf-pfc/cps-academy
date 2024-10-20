3b0e2c1730c7a122462cf115bb2199c2e71297c3
maruf-pfc <mdmarufsarker.mms@gmail.com>
2024-10-20 20:14:50 +0600

    Add: Awards in Commons, Fix: Redesign 2 pages

--- src/app/(demo)/commons/page.tsx
+++ src/app/(demo)/commons/page.tsx
@@ -33,7 +33,73 @@ export default function Commons() {
           </BreadcrumbItem>
         </BreadcrumbList>
       </Breadcrumb>
-      <div className="my-8 flex gap-4 flex-wrap">
+      <div className="my-8 flex gap-4 flex-col flex-wrap w-full md:w-1/2 lg:w-1/2 xl:w-1/2">
+        <h2 className="text-2xl text-orange-600 border-b-white border-b py-2 my-2">
+          Awards
+        </h2>
+        <Button asChild>
+          <Link
+            href="https://docs.google.com/document/d/1HuTh-2hKTMvJiY_nau2Pkd-OK9mDwpTk75s4T35z_IQ/edit?usp=sharing"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Pupil
+          </Link>
+        </Button>
+        <Button asChild>
+          <Link
+            href="https://docs.google.com/document/d/1RYORdvnwoSyn__hQ2j5H0uJhcs0cqm89rymfsIBPAx0/edit?usp=sharing"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Specialist
+          </Link>
+        </Button>
+        <Button asChild>
+          <Link
+            href="https://docs.google.com/document/d/1zYKa_Wpqoi-SZbqsv44zooN0j3MgQ8u3dkATkAmUblQ/edit?usp=sharing"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Expert
+          </Link>
+        </Button>
+        <h2 className="text-2xl text-orange-600 border-b-white border-b py-2 my-2">
+          Discord
+        </h2>
+        <Button asChild>
+          <Link
+            href="https://discord.com/channels/939568027597406288/1206310259996491826"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Discord Server
+          </Link>
+        </Button>
+        <h2 className="text-2xl text-orange-600 border-b-white border-b py-2 my-2">
+          Drive & Classroom
+        </h2>
+        <Button asChild>
+          <Link
+            href="https://drive.google.com/drive/u/0/folders/1TWpwYAIuP37mh_OLt9vHOFLwSOZmzthY"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            CPS Contents
+          </Link>
+        </Button>
+        <Button asChild>
+          <Link
+            href="https://classroom.google.com"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Google Classroom
+          </Link>
+        </Button>
+        <h2 className="text-2xl text-orange-600 border-b-white border-b py-2 my-2">
+          Facebook
+        </h2>
         <Button asChild>
           <Link
             href="https://business.facebook.com/latest/home?asset_id=103542411167326"
@@ -41,15 +107,6 @@ export default function Commons() {
             rel="noopener noreferrer"
           >
             Meta Business Suite
-          </Link>
-        </Button>
-        <Button asChild>
-          <Link
-            href="https://vjudge.net/groups"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Vjudge Groups
           </Link>
         </Button>
         <Button asChild>
@@ -70,24 +127,9 @@ export default function Commons() {
             Facebook Page
           </Link>
         </Button>
-        <Button asChild>
-          <Link
-            href="https://studio.youtube.com/channel/UC3S_jVMwhUrF6jjkFCQvyuA?c=UC3S_jVMwhUrF6jjkFCQvyuA"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            YouTube Studio
-          </Link>
-        </Button>
-        <Button asChild>
-          <Link
-            href="https://discord.com/channels/939568027597406288/1206310259996491826"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Discord Server
-          </Link>
-        </Button>
+        <h2 className="text-2xl text-orange-600 border-b-white border-b py-2 my-2">
+          Linkedin
+        </h2>
         <Button asChild>
           <Link
             href="https://linkedin.com/company/cps-academy"
@@ -97,22 +139,28 @@ export default function Commons() {
             Linkedin
           </Link>
         </Button>
+        <h2 className="text-2xl text-orange-600 border-b-white border-b py-2 my-2">
+          Vjudge
+        </h2>
         <Button asChild>
           <Link
-            href="https://drive.google.com/drive/u/0/folders/1TWpwYAIuP37mh_OLt9vHOFLwSOZmzthY"
+            href="https://vjudge.net/groups"
             target="_blank"
             rel="noopener noreferrer"
           >
-            CPS Contents
+            Vjudge Groups
           </Link>
         </Button>
+        <h2 className="text-2xl text-orange-600 border-b-white border-b py-2 my-2">
+          YouTube
+        </h2>
         <Button asChild>
           <Link
-            href="https://classroom.google.com"
+            href="https://studio.youtube.com/channel/UC3S_jVMwhUrF6jjkFCQvyuA?c=UC3S_jVMwhUrF6jjkFCQvyuA"
             target="_blank"
             rel="noopener noreferrer"
           >
-            Google Classroom
+            YouTube Studio
           </Link>
         </Button>
       </div>
--- src/app/(demo)/outlines/page.tsx
+++ src/app/(demo)/outlines/page.tsx
@@ -33,8 +33,11 @@ export default function Outlines() {
           </BreadcrumbItem>
         </BreadcrumbList>
       </Breadcrumb>
-      <div className="my-8 flex gap-4 flex-wrap w-full">
-        <Button asChild className="lg:w-1/2">
+      <div className="my-8 flex gap-4 flex-col flex-wrap w-full md:w-1/2 lg:w-1/2 xl:w-1/2">
+        <h2 className="text-2xl text-orange-600 border-b-white border py-2 my-2 border-t-0 border-l-0 border-r-0">
+          Batch 1
+        </h2>
+        <Button asChild>
           <Link
             href="https://docs.google.com/document/d/1fQh13gouu1m46GLrEUIu0smmBGd_l4shJSOnVK1kloE/edit?usp=sharing"
             target="_blank"
@@ -43,8 +46,11 @@ export default function Outlines() {
             Batch 1 Course Outline
           </Link>
         </Button>
-        <div className="flex flex-row gap-4 w-full flex-wrap">
-          <Button asChild className="lg:w-1/2">
+        <h2 className="text-2xl text-orange-600 border-b-white border py-2 my-2 border-t-0 border-l-0 border-r-0">
+          Batch 2
+        </h2>
+        <div className="flex flex-col gap-4 w-full flex-wrap">
+          <Button asChild>
             <Link
               href="https://docs.google.com/document/d/10pbqt3AluGKGGUPn_64hvfQKtl1sCQFM/edit?usp=sharing&ouid=102116855431773140531&rtpof=true&sd=true"
               target="_blank"
@@ -53,7 +59,7 @@ export default function Outlines() {
               Batch 2 (New) Course Outline
             </Link>
           </Button>
-          <Button asChild className="lg:w-1/2">
+          <Button asChild>
             <Link
               href="https://docs.google.com/document/d/1IJ-_A6lV_K3Hl_Mo6EX8DmwiFSL7AYS1raBKODI9c1Q/edit?usp=sharing"
               target="_blank"
@@ -63,8 +69,11 @@ export default function Outlines() {
             </Link>
           </Button>
         </div>
-        <div className="flex flex-row gap-4 w-full flex-wrap">
-          <Button asChild className="lg:w-1/2">
+        <h2 className="text-2xl text-orange-600 border-b-white border py-2 my-2 border-t-0 border-l-0 border-r-0">
+          Batch 3
+        </h2>
+        <div className="flex flex-col gap-4 w-full flex-wrap">
+          <Button asChild>
             <Link
               href="https://docs.google.com/document/d/1F1yEsAltIFZfbufBe-mAJnoXAkM-egqud6X8aMmWdhE/edit?usp=sharing"
               target="_blank"
@@ -73,7 +82,7 @@ export default function Outlines() {
               Batch 3 Course Outline
             </Link>
           </Button>
-          <Button asChild className="lg:w-1/2">
+          <Button asChild>
             <Link
               href="https://youtu.be/leZgGkt0IZI"
               target="_blank"
@@ -83,8 +92,11 @@ export default function Outlines() {
             </Link>
           </Button>
         </div>
-        <div className="flex flex-row gap-4 w-full flex-wrap">
-          <Button asChild className="lg:w-1/2">
+        <h2 className="text-2xl text-orange-600 border-b-white border py-2 my-2 border-t-0 border-l-0 border-r-0">
+          Batch 4
+        </h2>
+        <div className="flex flex-col gap-4 w-full flex-wrap">
+          <Button asChild>
             <Link
               href="https://docs.google.com/document/d/1F1yEsAltIFZfbufBe-mAJnoXAkM-egqud6X8aMmWdhE/edit?usp=sharing"
               target="_blank"
@@ -93,7 +105,7 @@ export default function Outlines() {
               Batch 4 Course Outline
             </Link>
           </Button>
-          <Button asChild className="lg:w-1/2">
+          <Button asChild>
             <Link
               href="https://youtu.be/2xZ2TMfUx7c"
               target="_blank"
